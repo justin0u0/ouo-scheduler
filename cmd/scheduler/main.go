939efd3d9b323ad1febd1bc25e1401489dfb0c86
justin0u0 <mail@justin0u0.com>
2020-08-30 21:22:37 +0800

    solve the version problems

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+  "fmt"
+  "os"
 	"math/rand"
 	"time"
+
+  "k8s.io/component-base/logs"
+	"github.com/justin0u0/ouo-scheduler/pkg/plugin"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	command := plugin.Register()
+
+  logs.InitLogs()
+  defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+    _, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+    os.Exit(1)
+	}
 }
--- pkg/plugin/register.go
+++ pkg/plugin/register.go
@@ -1,13 +1,11 @@
 package plugin
 
 import (
-  "github.com/spf13/cobra"
-  "k8s.io/kubernetes/cmd/kube-scheduler/app"
-  "github.com/justin0u0/ouo-scheduler/pkg/plugin/ouo"
+	"github.com/justin0u0/ouo-scheduler/pkg/plugin/ouo"
+	"github.com/spf13/cobra"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 )
 
 func Register() *cobra.Command {
-  return app.NewSchedulerCommand(
-    app.WithPlugin(ouo.Name, ouo.New)
-  )
+	return app.NewSchedulerCommand(app.WithPlugin(ouo.Name, ouo.New))
 }
