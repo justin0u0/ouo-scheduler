596d736a4fde8093f56176bbfde07fd48ef1020e
justin0u0 <mail@justin0u0.com>
2020-09-01 04:44:14 +0800

    Implement PreFilter plugin & update k8s version to v1.19.0

--- pkg/plugin/ouo/scheduler.go
+++ pkg/plugin/ouo/scheduler.go
@@ -1,23 +1,28 @@
 package ouo
 
 import (
+	"context"
+	"strconv"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog"
 	"k8s.io/kubernetes/pkg/api/v1/pod"
 	"k8s.io/kubernetes/pkg/apis/core/v1/helper/qos"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
-
-	"fmt"
 )
 
 // Name ... the custom shceduler name
 const Name = "ouo-scheduler"
 
 // CustomScheduler ... The type CustomScheduler implement the interface of the kube-scheduler framework
-type CustomScheduler struct{}
+type CustomScheduler struct {
+	handle framework.FrameworkHandle
+}
 
-// Let the type CustomScheduler implement the QueueSortPlugin interface
+// Let the type CustomScheduler implement the QueueSortPlugin, PreFilterPlugin interface
 var _ framework.QueueSortPlugin = &CustomScheduler{}
+var _ framework.PreFilterPlugin = &CustomScheduler{}
 
 // Name ... Implement Plugin interface Name() @pkg/scheduler/framework/v1alpha1/interface.go
 func (*CustomScheduler) Name() string {
@@ -25,11 +30,11 @@ func (*CustomScheduler) Name() string {
 }
 
 // Less ... Implement QueueSortPlugin interface Less() @pkg/scheduler/framework/v1alpha1/interface.go
-func (*CustomScheduler) Less(pInfo1, pInfo2 *framework.PodInfo) bool {
+func (*CustomScheduler) Less(pInfo1, pInfo2 *framework.QueuedPodInfo) bool {
 	p1 := pod.GetPodPriority(pInfo1.Pod)
 	p2 := pod.GetPodPriority(pInfo2.Pod)
 
-	fmt.Printf("[LESS: ] %v: %v, %v: %v\n", pInfo1.Pod.Name, p1, pInfo2.Pod.Name, p2)
+	klog.V(3).Infof("[queue sort] [Less]: %v: %v, %v: %v", pInfo1.Pod.Name, p1, pInfo2.Pod.Name, p2)
 
 	return (p1 > p2) || (p1 == p2 && comparePodQOS(pInfo1.Pod, pInfo2.Pod))
 }
@@ -43,7 +48,7 @@ func comparePodQOS(pod1, pod2 *v1.Pod) bool {
 	pod1QOS := qos.GetPodQOS(pod1)
 	pod2QOS := qos.GetPodQOS(pod2)
 
-	fmt.Printf("[QOS: ] %v: %v, %v: %v\n", pod1.Name, pod1QOS, pod2.Name, pod2QOS)
+	klog.V(3).Infof("[queue sort] [comparePodQOS]: %v: %v, %v, %v", pod1.Name, pod1QOS, pod2.Name, pod2QOS)
 
 	if pod1QOS == v1.PodQOSGuaranteed {
 		return true
@@ -54,9 +59,58 @@ func comparePodQOS(pod1, pod2 *v1.Pod) bool {
 	return pod1QOS == pod2QOS
 }
 
+// PreFilter ... Implement PreFilterPlugin interface PreFilter()
+func (s *CustomScheduler) PreFilter(_ context.Context, _ *framework.CycleState, pod *v1.Pod) *framework.Status {
+	nodeInfos, err := s.handle.SnapshotSharedLister().NodeInfos().List()
+	if err != nil {
+		return framework.NewStatus(framework.Unschedulable, "Failed to pass pre filter, get nodeInfos failed.")
+	}
+
+	podGroup, podGroupExist := pod.Labels["podGroup"]
+	if !podGroupExist {
+		return framework.NewStatus(framework.Success, "Pass pre filter successfully, pod has no label podGroup.")
+	}
+
+	minAvailable, minAvailableExist := pod.Labels["minAvailable"]
+	if !minAvailableExist {
+		return framework.NewStatus(framework.Success, "Pass pre filter successfully, pod has no label minAvailable.")
+	}
+	minAvailableNum, atoiErr := strconv.Atoi(minAvailable)
+	if atoiErr != nil {
+		return framework.NewStatus(framework.Unschedulable, "Failed to pass pre filter, pod label minAvailable is not a valid number")
+	}
+
+	totalPodsInPodGroup := getTotalPodsByPodGroup(nodeInfos, pod.Namespace, podGroup)
+	if totalPodsInPodGroup < minAvailableNum {
+		klog.V(3).Infof("The count of PodGroup %v (%v) is less than minAvailable(%d) in PreFilter: %d", podGroup, pod.Name, minAvailableNum, totalPodsInPodGroup)
+		return framework.NewStatus(framework.Unschedulable, "Failed to pass pre filter, less than min available")
+	}
+
+	return framework.NewStatus(framework.Success, "Pass pre filter successfully")
+}
+
+// PreFilterExtensions ...
+func (*CustomScheduler) PreFilterExtensions() framework.PreFilterExtensions {
+	return nil
+}
+
+func getTotalPodsByPodGroup(nodeInfos []*framework.NodeInfo, ns string, pg string) int {
+	total := 0
+	for _, nodeInfo := range nodeInfos {
+		for _, podInfo := range nodeInfo.Pods {
+			if podGroup, ok := podInfo.Pod.Labels["podGroup"]; ok && podGroup == pg && podInfo.Pod.Namespace == ns {
+				total++
+			}
+		}
+	}
+	return total
+}
+
 // New ... Create an scheduler instance
 // New() is type PluginFactory = func(configuration runtime.Object, f v1alpha1.FrameworkHandle) (v1alpha1.Plugin, error)
 // mentioned in https://github.com/kubernetes/kubernetes/blob/master/pkg/scheduler/framework/runtime/registry.go
-func New(_ *runtime.Unknown, _ framework.FrameworkHandle) (framework.Plugin, error) {
-	return &CustomScheduler{}, nil
+func New(_ *runtime.Unknown, handle framework.FrameworkHandle) (framework.Plugin, error) {
+	return &CustomScheduler{
+		handle: handle,
+	}, nil
 }
